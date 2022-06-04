fc0072fecf4715bb538a4e455954c5ae7e89a2fa
Hulxv <m7mdx37@gmail.com>
2022-06-04 06:04:23 +0200

    [feat] add description for possible values

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use clap::{ArgEnum, Parser, Subcommand};
+use clap::{ArgEnum, Args as ArgsMacro, Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 pub struct Args {
@@ -17,19 +17,47 @@ pub enum Commands {
     },
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum, Debug, Subcommand)]
 pub enum ServerCommands {
-    Run,
+    /// Shutdown server.
+    /// You will need to restart vns daemon to running the server again.
     Shutdown,
-    Restart,
+    /// Get server status
+    Status,
+    /// Pause accepting incoming connections.
+    ///May drop socket pending connection. All open connections remain active.
+    Pause,
+    /// Resume accepting incoming connections.
+    Resume,
 }
+
+#[derive(ArgsMacro, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+/// Run youer server
+struct Run {}
 
 impl Display for ServerCommands {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ServerCommands::Run => write!(f, "run"),
-            ServerCommands::Restart => write!(f, "restart"),
+            // ServerCommands::Run {} => write!(f, "run"),
+            // ServerCommands::Restart => write!(f, "restart"),
             ServerCommands::Shutdown => write!(f, "shutdown"),
+            ServerCommands::Status => write!(f, "status"),
+            ServerCommands::Pause => write!(f, "pause"),
+            ServerCommands::Resume => write!(f, "resume"),
+        }
+    }
+}
+impl std::str::FromStr for ServerCommands {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            // "run" => Ok(ServerCommands::Run ),
+            // "restart" => Ok(ServerCommands::Restart),
+            "shutdown" => Ok(ServerCommands::Shutdown),
+            "status" => Ok(ServerCommands::Status),
+            "pause" => Ok(ServerCommands::Pause),
+            "resume" => Ok(ServerCommands::Resume),
+            _ => Err("invalid"),
         }
     }
 }
