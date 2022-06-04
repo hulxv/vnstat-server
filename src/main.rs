5575ac9ae5d67ec02c1ec403d58286ae742e8ddc
Hulxv <m7mdx37@gmail.com>
2022-06-04 06:11:04 +0200

    [feat] setup uds client in vns

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,88 @@
 use app::log::Logger;
 use clap::Parser;
 use log::{error, warn};
-use tokio::net::UnixStream;
+use std::{str::FromStr, string::ToString};
+use utils::unix_socket::{Message, UnixSocket};
 use vns::cli::{Args, Commands, ServerCommands};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     Logger::init();
 
-    let stream = UnixStream::connect("/tmp/vns.socket").await?;
+    let mut socket = UnixSocket::connect("/tmp/vns.socket")
+        .await
+        .map_err(|e| error!("{e}"))
+        .expect("");
 
     match args.commands {
-        Some(Commands::Server { command }) => {
-            send_message(&stream, format!("{command}").as_str())
+        Some(Commands::Server { command }) => loop {
+            match socket
+                .send(
+                    Message::from_str(format!("{command}").as_str())
+                        .unwrap()
+                        .to_string()
+                        .as_str(),
+                )
                 .await
-                .map_err(|e| error!("{e}"))
-                .unwrap();
-        }
+            {
+                Err(ref e)
+                    if e.root_cause()
+                        .downcast_ref::<std::io::Error>()
+                        .unwrap()
+                        .kind()
+                        == std::io::ErrorKind::WouldBlock =>
+                {
+                    continue;
+                }
+                Err(e) => error!("{e}"),
+                Ok(_) => match command {
+                    ServerCommands::Shutdown => {
+                        warn!("Shutdown server gracefully, you will need to restart vns daemon to re-running http server");
+                    }
+                    ServerCommands::Status => loop {
+                        match socket.receive().await {
+                            Ok(message) => {
+                                println!("{message:#?}",);
+                                break;
+                            }
+                            Err(ref e)
+                                if e.root_cause()
+                                    .downcast_ref::<std::io::Error>()
+                                    .unwrap()
+                                    .kind()
+                                    == std::io::ErrorKind::WouldBlock =>
+                            {
+                                continue;
+                            }
+                            Err(e) => error!("{e}"),
+                        }
+                    },
+                    _ => (),
+                },
+            };
+            break;
+        },
         _ => {
             warn!("use --help flag to show available commands")
         }
-    };
-
-    Ok(())
-}
-
-async fn send_message(stream: &UnixStream, message: &str) -> Result<(), std::io::Error> {
-    loop {
-        stream.writable().await?;
-        match stream.try_write(message.as_bytes()) {
-            Ok(n) => {
-                break;
-            }
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                continue;
-            }
-            Err(e) => return Err(e),
-        };
     }
+
     Ok(())
 }
+
+// async fn send_message(stream: &UnixStream, message: &str) -> Result<(), std::io::Error> {
+//     loop {
+//         stream.writable().await?;
+//         match stream.try_write(message.as_bytes()) {
+//             Ok(n) => {
+//                 break;
+//             }
+//             Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+//                 continue;
+//             }
+//             Err(e) => return Err(e),
+//         };
+//     }
+//     Ok(())
+// }
