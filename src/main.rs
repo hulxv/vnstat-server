1290df67b4d41c0daf7268d23afa5acb1f3662f9
Hulxv <m7mdx37@gmail.com>
2022-06-10 03:41:23 +0200

    [fix] server recieves invalid message when use vns without args

--- src/main.rs
+++ src/main.rs
@@ -17,160 +17,162 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     Logger::init();
 
-    let socket = UnixSocket::connect("/tmp/vns.socket")
-        .await
-        .map_err(|e| error!("{e}"))
-        .expect("");
-
     match args.commands {
-        Some(Commands::Server { command }) => loop {
-            match socket
-                .send(
-                    Message::from_str(format!("{command}").as_str())
-                        .unwrap()
-                        .to_string()
-                        .as_str(),
-                )
+        Some(Commands::Server { command }) => {
+            let socket = UnixSocket::connect("/tmp/vns.socket")
                 .await
-            {
-                Err(ref e)
-                    if e.root_cause()
-                        .downcast_ref::<std::io::Error>()
-                        .unwrap()
-                        .kind()
-                        == std::io::ErrorKind::WouldBlock =>
+                .map_err(|e| error!("{e}"))
+                .expect("");
+
+            loop {
+                match socket
+                    .send(
+                        Message::from_str(format!("{command}").as_str())
+                            .unwrap()
+                            .to_string()
+                            .as_str(),
+                    )
+                    .await
                 {
-                    continue;
-                }
-                Err(e) => error!("{e}"),
-                Ok(_) => match command {
-                    Resume | Pause | Shutdown => {
-                        if command == Shutdown {
-                            warn!("Shutdown server gracefully, you will need to restart vns daemon to re-running http server");
-                        }
-                        select!(
-                            _ = async {
-                                loop {
-
-                                    match socket.receive().await {
-                                        Err(ref e)
-                                            if e.root_cause()
-                                                .downcast_ref::<std::io::Error>()
-                                                .unwrap()
-                                                .kind()
-                                                == std::io::ErrorKind::WouldBlock =>
-                                        {
-                                            continue;
-                                        }
-                                        Err(e) => error!("{e}"),
-                                        Ok(message) => {
-                                            let hash: HashMap<String, String> = serde_json::from_str(message.as_str()).unwrap();
-                                            println!("{}: {}",
-                                                match hash.get("status").unwrap().to_lowercase().as_str(){
-                                                    "failed" => "Failed".red().bold(),
-                                                    "success" => "Success".green().bold(),
-                                                    e => e.bold()
-                                                },
-                                                hash.get("details").unwrap()
-                                            );
-                                        }
-                                    }
-                                    break;
-
-                                }
-                            } => {}
-                            _ = async {
-                                time::sleep(Duration::from_millis(4000)).await;
-                            } => {}
-                        );
+                    Err(ref e)
+                        if e.root_cause()
+                            .downcast_ref::<std::io::Error>()
+                            .unwrap()
+                            .kind()
+                            == std::io::ErrorKind::WouldBlock =>
+                    {
+                        continue;
                     }
-                    Status => select!(_ = async{loop {
-                            match socket.receive().await {
-                                Err(ref e)
-                                    if e.root_cause()
-                                        .downcast_ref::<std::io::Error>()
-                                        .unwrap()
-                                        .kind()
-                                        == std::io::ErrorKind::WouldBlock =>
-                                {
-                                    continue;
-                                }
-                                Err(e) => error!("{e}"),
-                                Ok(message) => {
-                                    let hash: HashMap<String, String> =
-                                        serde_json::from_str(message.as_str()).unwrap();
-
-                                    let (ip, port) =
-                                        (hash.get("ip").unwrap(), hash.get("port").unwrap());
-
-                                    match hash.get("status").unwrap().to_lowercase().as_str() {
-                                        "active" => {
-                                            println!(
-                                                "{} ({}) {}:",
-                                                "Active".green().bold(),
-                                                "Running".yellow().bold(),
-                                                "on".bold(),
-                                            );
-                                            println!(
-                                                "{: >1}  {}",
-                                                "",
-                                                format!("{:<5} {}", "IP", ip.yellow(),).bold()
-                                            );
-                                            println!(
-                                                "{: >1}  {}",
-                                                "",
-                                                format!("{:<5} {}", "PORT", port.yellow()).bold()
-                                            );
-                                        }
-                                        "idle" => {
-                                            println!(
-                                                "{} ({})",
-                                                "Idle".cyan().bold(),
-                                                "Paused".blue().bold(),
-                                            );
-
-                                            println!(
-                                                "\n{} ",
-                                                format!(
-                                                    "{}\n {} \n{}:  \n $ {}",
-                                                    "Note".bright_blue(),
-                                                    "To resume incoming connections",
-                                                    "run".yellow(),
-                                                    "vns server resume",
-                                                )
-                                                .bold()
-                                            )
-                                        }
-                                        "inactive" => {
-                                            println!(
-                                                "{} ({})",
-                                                "Inactive".yellow().bold(),
-                                                "STOPPED".red().bold(),
-                                            );
-                                            println!(
-                                                "\n{}\n {}",
-                                                "Note".bright_blue().bold(),
-                                                format!(
-                                                    "Restart {} to re-running the server.",
-                                                    "vns daemon".yellow(),
-                                                )
-                                            )
-                                        }
-                                        _ => (),
-                                    };
-                                }
+                    Err(e) => error!("{e}"),
+                    Ok(_) => match command {
+                        Resume | Pause | Shutdown => {
+                            if command == Shutdown {
+                                warn!("Shutdown server gracefully, you will need to restart vns daemon to re-running http server");
                             }
-                            break;
-                        }} => {},
-                    _ = async{
-                            time::sleep(Duration::from_millis(4000)).await;
-                        } => error!("Cannot recieve server status: connection timeout")
-                    ),
-                },
-            };
-            break;
-        },
-        _ => {
+                            select!(
+                                _ = async {
+                                    loop {
+
+                                        match socket.receive().await {
+                                            Err(ref e)
+                                                if e.root_cause()
+                                                    .downcast_ref::<std::io::Error>()
+                                                    .unwrap()
+                                                    .kind()
+                                                    == std::io::ErrorKind::WouldBlock =>
+                                            {
+                                                continue;
+                                            }
+                                            Err(e) => error!("{e}"),
+                                            Ok(message) => {
+                                                let hash: HashMap<String, String> = serde_json::from_str(message.as_str()).unwrap();
+                                                println!("{}: {}",
+                                                    match hash.get("status").unwrap().to_lowercase().as_str(){
+                                                        "failed" => "Failed".red().bold(),
+                                                        "success" => "Success".green().bold(),
+                                                        e => e.bold()
+                                                    },
+                                                    hash.get("details").unwrap()
+                                                );
+                                            }
+                                        }
+                                        break;
+
+                                    }
+                                } => {}
+                                _ = async {
+                                    time::sleep(Duration::from_millis(4000)).await;
+                                } => {}
+                            );
+                        }
+                        Status => select!(_ = async{loop {
+                                match socket.receive().await {
+                                    Err(ref e)
+                                        if e.root_cause()
+                                            .downcast_ref::<std::io::Error>()
+                                            .unwrap()
+                                            .kind()
+                                            == std::io::ErrorKind::WouldBlock =>
+                                    {
+                                        continue;
+                                    }
+                                    Err(e) => error!("{e}"),
+                                    Ok(message) => {
+                                        let hash: HashMap<String, String> =
+                                            serde_json::from_str(message.as_str()).unwrap();
+
+                                        let (ip, port) =
+                                            (hash.get("ip").unwrap(), hash.get("port").unwrap());
+
+                                        match hash.get("status").unwrap().to_lowercase().as_str() {
+                                            "active" => {
+                                                println!(
+                                                    "{} ({}) {}:",
+                                                    "Active".green().bold(),
+                                                    "Running".yellow().bold(),
+                                                    "on".bold(),
+                                                );
+                                                println!(
+                                                    "{: >1}  {}",
+                                                    "",
+                                                    format!("{:<5} {}", "IP", ip.yellow(),).bold()
+                                                );
+                                                println!(
+                                                    "{: >1}  {}",
+                                                    "",
+                                                    format!("{:<5} {}", "PORT", port.yellow()).bold()
+                                                );
+                                            }
+                                            "idle" => {
+                                                println!(
+                                                    "{} ({})",
+                                                    "Idle".cyan().bold(),
+                                                    "Paused".blue().bold(),
+                                                );
+
+                                                println!(
+                                                    "\n{} ",
+                                                    format!(
+                                                        "{}\n {} \n{}:  \n $ {}",
+                                                        "Note".bright_blue(),
+                                                        "To resume incoming connections",
+                                                        "run".yellow(),
+                                                        "vns server resume",
+                                                    )
+                                                    .bold()
+                                                )
+                                            }
+                                            "inactive" => {
+                                                println!(
+                                                    "{} ({})",
+                                                    "Inactive".yellow().bold(),
+                                                    "STOPPED".red().bold(),
+                                                );
+                                                println!(
+                                                    "\n{}\n {}",
+                                                    "Note".bright_blue().bold(),
+                                                    format!(
+                                                        "Restart {} to re-running the server.",
+                                                        "vns daemon".yellow(),
+                                                    )
+                                                )
+                                            }
+                                            _ => (),
+                                        };
+                                    }
+                                }
+                                break;
+                            }} => {},
+                        _ = async{
+                                time::sleep(Duration::from_millis(4000)).await;
+                            } => error!("Cannot recieve server status: connection timeout")
+                        ),
+                    },
+                };
+                break;
+            }
+        }
+        None => {
             warn!("use --help flag to show available commands")
         }
     }
