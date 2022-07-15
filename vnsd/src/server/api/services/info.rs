aea2dcbf9b4a12732f59620c2431e7d1ef096704
Hulxv <m7mdx37@gmail.com>
2022-07-15 21:58:43 +0200

    [feat] add vns-version to "/api/info" response

--- vnsd/src/server/api/services/info.rs
+++ vnsd/src/server/api/services/info.rs
@@ -1,17 +1,24 @@
 use crate::http::response::{Response, ResponseError, ResponseStatus};
 use actix_web::{get, HttpResponse};
-use libvnstat::VnStat;
+use libvnstat::{db::models::Info, VnStat};
 use log::error;
 
 #[get("/info")]
 pub async fn get_info() -> HttpResponse {
     match VnStat.info().get() {
-        Ok(result) => HttpResponse::Ok().json(
-            Response::new()
-                .status(ResponseStatus::Success)
-                .data(&result)
-                .build(),
-        ),
+        Ok(mut result) => {
+            result.push(Info {
+                id: result.len() as i32 + 1,
+                name: "vns-version".to_owned(),
+                value: env!("CARGO_PKG_VERSION").to_owned(),
+            });
+            HttpResponse::Ok().json(
+                Response::new()
+                    .status(ResponseStatus::Success)
+                    .data(&result)
+                    .build(),
+            )
+        }
 
         Err(err) => {
             error!("{err}");
